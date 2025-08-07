92167ccc4cad5575e2b7080654787f1d304e87f4
rayman202 <144568241+rayman202@users.noreply.github.com>
2025-08-06 22:56:01 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
     `java-library`
     `maven-publish`
+    kotlin("jvm") version "1.9.10"
 }
 
 group = "com.github.rayman202"
 version = "1.0.0"
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+}
 
 publishing {
     publications {
